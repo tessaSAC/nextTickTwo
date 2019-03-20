4cb3be25f59b2cab21985870ef45df2c546a5f86
tessaSAC <tessaSAC@users.noreply.github.com>
2019-03-20 12:44:51 -0400

    cleans up DOM accessors – seems to result in many more rAF calls fsr?

--- src/views/DeLorean.vue
+++ src/views/DeLorean.vue
@@ -38,63 +38,42 @@ export default {
       textColor: '#f8ef4f',
       labelText: 'time last departed',
     },
-
-    lcdCommonSelector: '#app > div.DeLorean.content > div.dashboard > div:nth-child(1) > div > div.lcds > div.DeLoreanLabeledSlot.DeLoreanCounter.',
   }),
 
-  computed: {
-    // wonder why this only gets updated once (why not never or all the time?)
-    // TODO: figure out a way to drill down via ref
-    // If this works I'll be annoyed because it worked both ways on NextTickVsRaf
-    // It didn't work
-    destinationHour() {
-      const tens = this.$refs.destination.$refs.minute.$refs.char[0].textContent
-      const ones = this.$refs.destination.$refs.minute.$refs.char[1].textContent  // this has the same issue where it only updates once
-      // '$refs are only populated after the component has been rendered, and they are not reactive. It is only meant as an escape hatch for direct child manipulation - you should avoid accessing $refs from within templates or computed properties.'
-      // So why does it work in NextTickVsRaf? Is it working? It does seem to be changing.....
-      // It doesn't work there anymore either (in methods). Agh....
-
-      return tens + ones
-    },
-  },
-
   methods: {
+    destinationHour() { return this.getPresentTime('hour') },
+
     destinationMinute() {
       console.log('theoretically via refs', this.$refs.destination.$refs.minute)
       console.log('idk what this is; is it the same?', this.$refs.destination.minute)
       console.log(this.$refs.destination.$refs.minute === this.$refs.destination.minute)  // no
 
-      console.log('char via refs', this.$refs.destination.$refs.minute.$refs.char)
-      console.log('theoretically via refs', this.$refs.destination.$refs.minute.$refs.char[0].textContent)
-      console.log('theoretically via refs', this.$refs.destination.$refs.minute.$refs.char[1].textContent)
+      return this.getPresentTime('minute')
+    },
 
-      const tens = this.getTextContent(document.querySelector(`${ this.lcdCommonSelector }minute > div > div.counter > div:nth-child(1) > div.opaque`))
-      const ones = this.getTextContent(document.querySelector(`${ this.lcdCommonSelector }minute > div > div.counter > div:nth-child(2) > div.opaque`))
+    getPresentTime(timeCounter) {
+      const tens = (this.$refs.destination.$refs[timeCounter].$refs.char[0].textContent)
+      const ones = (this.$refs.destination.$refs[timeCounter].$refs.char[1].textContent)
 
       return tens + ones
     },
 
-    getPresentTime() {
-      console.log(this.destinationMinute() + '')
+    setPresentTime() {
       Object.assign(this.present, {
-        hour: this.destinationHour,
+        hour: this.destinationHour(),
         minute: this.destinationMinute(),
       })
     },
 
-    getTextContent(node) {
-      return node.textContent  // this doesn't reflect what's on the page!
-    },
-
     nextTickToRaf() {
       this.promisedNextTick(this.travel)
-      .then(_ => this.promisedRequestAnimationFrame(this.getPresentTime))
+      .then(_ => this.promisedRequestAnimationFrame(this.setPresentTime))
       .catch(console.error)
     },
 
     rafToNextTick() {
       this.promisedRequestAnimationFrame(this.travel)  // fsr a separate nextTick is being triggered before $nextTick
-      .then(_ => this.promisedNextTick(this.getPresentTime))  // but that happens in the other example too
+      .then(_ => this.promisedNextTick(this.setPresentTime))  // but that happens in the other example too
       .catch(console.error)
     },
 
