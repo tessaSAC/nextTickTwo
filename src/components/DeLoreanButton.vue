f79bd5db77e15a0d64b53cf62f3b706d99fecd28
tessaSAC <tessaSAC@users.noreply.github.com>
2019-03-20 17:24:11 -0400

    creates global BttF sass variables

--- src/components/DeLoreanButton.vue
+++ src/components/DeLoreanButton.vue
@@ -10,10 +10,10 @@
 <style lang="scss">
 .DeLoreanButton {
   border-radius: 50px;
-  border: 1px solid #e06b2a;
+  border: 1px solid #$orange;
   background: transparent;
   padding: 0.4rem 0.8rem 0.3rem;
-  color: #e06b2a;
+  color: #$orange;
   font-family: Syncopate;
   font-size: 1.2rem;
   text-transform: uppercase;
