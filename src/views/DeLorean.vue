1f0e6b07f361cc2eb86a109ee5d1620f5196fd04
tessaSAC <tessaSAC@users.noreply.github.com>
2019-03-22 01:34:55 -0400

    adds more experiments with nesting and timeouts

--- src/views/DeLorean.vue
+++ src/views/DeLorean.vue
@@ -2,6 +2,7 @@
 import DeLoreanButton from '../components/DeLoreanButton'
 import DeLoreanClock from '../components/DeLoreanClock'
 import promisedMethods from '../mixins/promisedMethods'
+import { setImmediate, } from 'timers'
 
 export default {
   components: { DeLoreanButton, DeLoreanClock, },
@@ -44,12 +45,14 @@ export default {
 
   methods: {
     changeNonRenderedData() {
-      this.promisedNextTick(_ => this.uselessParam = 'geiss')  // flushes queue but doesn't trigger DOM patch
-      .then(_ => {
-        console.log('finsished nextTick phase')
+      console.log('nonDep func running')
+      // this.promisedNextTick(_ => this.uselessParam = 'geiss')  // flushes queue but doesn't trigger DOM patch
+      // .then(_ => {
+      //   console.log('finsished nextTick phase')
 
         window.requestAnimationFrame(_ => this.uselessParam = null)  // this also doesn't trigger DOM patch
-      })
+      // })
+      console.log('nonDep func end')
     },
 
     destinationHour() { return this.getPresentTime('hour') },
@@ -68,6 +71,48 @@ export default {
         hour: this.destinationHour(),
         minute: this.destinationMinute(),
       })
+    },
+
+    setImmediate(...args) {
+      console.log('running setImmediate')
+      return setImmediate(...args)
+    },
+
+    setTimeout(cb, timeout) {
+      console.log('running settimeout')
+      return setTimeout(cb, timeout)
+    },
+
+    prom() {
+      console.log('running promise')
+      return Promise.resolve().then(console.log('resolved promise'))
+    },
+
+    interceptQueue() {
+      // Because behavior is different when not deeply nested as in rafToNextTick
+      this.$nextTick(_ => {
+        console.log('interceptQueue: running $nextTick')  // flush 01
+        this.setImmediate(_ => { console.log('this.$nextTick(setImmediate)') }, (0))  // after final flush
+
+        this.setImmediate(this.$nextTick(_ => console.log('this.$nextTick(setImmediate(this.$nextTick))')))  // flush 02
+        this.setTimeout(_ => console.log('in setTimeout'), 0)  // after final flush
+
+        this.$nextTick(_ => console.log('INNER $NEXTTICK'))  // flush 02
+        this.$nextTick(_ => this.prom().then(_ => console.log('nested this.$nextTick => Promise.then')))  // after final flush?
+        this.$nextTick(_ => this.$nextTick(this.$nextTick))  // let's see...
+
+        this.prom().then(_ => console.log('Promise.then'))  // after final flush before prev. line maybe bc it gets into microtask queue one step faster?
+      })
+
+      this.setImmediate(_ => { console.log('in setImmediate') }, (0))  // after final
+      this.setImmediate(this.$nextTick(_ => console.log('setImmediate(this.$nextTick)')))  // INSIDE flush 01?!??
+
+      // CONCLUSIONS:
+      // 1. timers run after flushes 👌
+      // 2. nextTick runs during flushes
+      //    output goes... (task) ? after flushes : in next flush
+      // 3. promise inside flushQueue is treated as a t̶a̶s̶k̶(̶!̶?̶)̶ normal non-nextTick microtask
+      // 4. nextTick called by timer runs BEFOR timer(!?)
     },
 
     nextTickToRaf() {
@@ -114,24 +159,53 @@ export default {
       // Trying to make it into the same queue flush phase
       window.requestAnimationFrame(_ => {
         window.requestAnimationFrame(_ => {
-          console.log('rafToNextTick 01: did I make it into the same queue?')
+          console.log('rafToNextTick 01: did I make it into the same queue?')  // these all run right away
 
           window.requestAnimationFrame(_ => {
             console.log('rafToNextTick 02: how about me?')
-            this.travel()
+            this.travel()  // unsurprisingly (and obviously in retrospect 😑) since the console.logs are showing right away function calls inside the callback run too, i.e. *** the callback runs immediately *** meaning NEXTTICK GETS PUSHED at the end of the function (task end(???))
+
+            // new theory: nextTick only gets pushed if dep value is changed (should be true since console.log doesn't cause this?)
+            // => YES
+            this.changeNonRenderedData()
+
             console.log('rafToNextTick 03: and me')
             this.$nextTick(this.setPresentTime)
             console.log('rafToNextTick 04: and me')  // these all print as the cbs get pushed
 
             // theory: this one will print inside the first flushQueue call:
+            // => YES
             this.$nextTick(_ => console.log('rafToNextTick 05: inside $nextTick'))
+
+            this.$nextTick(_ => {
+              console.log('rafToNextTick 06: inside $nextTick before timeout')
+              setImmediate(_ => { console.log('in timeout') }, (0))  // whether long or short, setImmediate or setTimeout, as a task it doesn't make it into the current queue
+              setImmediate(this.$nextTick(_ => console.log('go in current queue?')))  // what if I schedule a microtask?
+              // This goes into the next queue
+
+              // If I nest nextTicks will they go into the same or different flushed queues?
+              // => separate queues!
+              // So it seems like you can't push into the current queue from within the queue
+              // Whether nextTick is running a Task or a Microtask it seems like it can't intercept the currently flushing queue
+              this.$nextTick(_ => {
+                console.log('OUTER $NEXTTICK')
+                this.$nextTick(_ => console.log('INNER $NEXTTICK'))
+              })
+              console.log('rafToNextTick 07: inside $nextTick after timeout')
+              // Try one more style just to be extra-sure => this comes outside the queue just like with setTimeout and setImmediate(!?)
+              this.$nextTick(_ => Promise.resolve().then(_ => console.log('inside resolved promise to be exxxxxtra sureeee')))
+              Promise.resolve().then(_ => console.log('ok how about this promise'))
+              // Whoa this one comes between queues wtf I guess because it's going into the microtask queue before nextTick? 🤔
+              // is this a separate microtask queue => YES
+            })
           })
         })
       })
     },
 
-    travel({ hour, minute } = {}) {  // making destructured args optional, e.g. https://stackoverflow.com/a/53930370
-      this.departed = { ...this.present }
+    travel({ hour, minute, } = {}) {  // making destructured args optional, e.g. https://stackoverflow.com/a/53930370
+      console.log('travel running')
+      this.departed = { ...this.present, }
 
       if(hour) this.destination.hour = hour
       if(minute) this.destination.minute = minute
@@ -153,6 +227,7 @@ export default {
     <DeLoreanButton @click="nextTickToRaf">nextTick</DeLoreanButton>
     <DeLoreanButton @click="rafToNextTick">rAF</DeLoreanButton>
     <DeLoreanButton @click="changeNonRenderedData">!rerender</DeLoreanButton>
+    <DeLoreanButton @click="interceptQueue">MI:Vue</DeLoreanButton>
   </div>
 </div>
 </template>
